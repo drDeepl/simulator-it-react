43ff8f59532589f9950cff23cce378137ad62fd2
drDeepl <Vlimaster@yandex.ru>
2024-10-16 15:42:53 +0700

    fix: removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { PrimeReactProvider } from "primereact/api";
 
 import "./assets/index.scss";
 
 import App from "./App";
-
-const loading = (
-  <div className="pt-3 text-center">
-    <div className="sk-spinner sk-spinner-pulse"></div>
-  </div>
-);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
